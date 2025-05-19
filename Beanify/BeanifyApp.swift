6326f51d956f80bfc0da5e76cdedc0dda708b244
mohamed <feituri4456@gmail.com>
2025-05-19 12:49:24 -0600

    Initial Commit

---
+++ Beanify/BeanifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeanifyApp.swift
+//  Beanify
+//
+//  Created by Mohamed Feituri on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BeanifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeanifyTests/BeanifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeanifyTests.swift
+//  BeanifyTests
+//
+//  Created by Mohamed Feituri on 5/19/25.
+//
+
+import Testing
+@testable import Beanify
+
+struct BeanifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
