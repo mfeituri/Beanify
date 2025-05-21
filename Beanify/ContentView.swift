401246631686197884cfdf6667d370a483058a9e
mohamed <feituri4456@gmail.com>
2025-05-21 12:01:23 -0600

    first commit

--- Beanify/ContentView.swift
+++ Beanify/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username: String = " "
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Form{
+            TextField("Username", text: $username ){
+                
+                
+            }
+            
+            
+            .padding()
         }
-        .padding()
     }
 }
 
